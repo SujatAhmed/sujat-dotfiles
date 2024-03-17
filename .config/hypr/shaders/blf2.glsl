a3595eafe8872621b4dc0d8b9c76357e30485fb3
sujat <sujatahmed841999@gmail.com>
2024-03-17 12:19:18 +0600

    changed the colors of the modules

--- .config/hypr/shaders/blf2.glsl
+++ .config/hypr/shaders/blf2.glsl
@@ -4,7 +4,7 @@ precision highp float;
 varying vec2 v_texcoord;
 uniform sampler2D tex;
 
-const float temperature = 3700.0;
+const float temperature = 4300.0;
 const float temperatureStrength = 1.0;
 
 #define WithQuickAndDirtyLuminancePreservation
@@ -15,13 +15,13 @@ const float LuminancePreservationFactor = 1.0;
 vec3 colorTemperatureToRGB(const in float temperature) {
     // values from: http://blenderartists.org/forum/showthread.php?270332-OSL-Goodness&p=2268693&viewfull=1#post2268693
     mat3 m = (temperature <= 6500.0) ? mat3(vec3(0.0, -2902.1955373783176, -8257.7997278925690),
-                                            vec3(0.0, 1669.5803561666639, 2575.2827530017594),
-                                            vec3(1.0, 1.3302673723350029, 1.8993753891711275))
-                                     : mat3(vec3(1745.0425298314172, 1216.6168361476490, -8257.7997278925690),
-                                            vec3(-2666.3474220535695, -2173.1012343082230, 2575.2827530017594),
-                                            vec3(0.55995389139931482, 0.70381203140554553, 1.8993753891711275));
+            vec3(0.0, 1669.5803561666639, 2575.2827530017594),
+            vec3(1.0, 1.3302673723350029, 1.8993753891711275))
+        : mat3(vec3(1745.0425298314172, 1216.6168361476490, -8257.7997278925690),
+                vec3(-2666.3474220535695, -2173.1012343082230, 2575.2827530017594),
+                vec3(0.55995389139931482, 0.70381203140554553, 1.8993753891711275));
     return mix(clamp(vec3(m[0] / (vec3(clamp(temperature, 1000.0, 40000.0)) + m[1]) + m[2]), vec3(0.0), vec3(1.0)),
-               vec3(1.0), smoothstep(1000.0, 0.0, temperature));
+            vec3(1.0), smoothstep(1000.0, 0.0, temperature));
 }
 
 void main() {
@@ -32,7 +32,7 @@ void main() {
 
 #ifdef WithQuickAndDirtyLuminancePreservation
     color *= mix(1.0, dot(color, vec3(0.2126, 0.7152, 0.0722)) / max(dot(color, vec3(0.2126, 0.7152, 0.0722)), 1e-5),
-                 LuminancePreservationFactor);
+            LuminancePreservationFactor);
 #endif
 
     color = mix(color, color * colorTemperatureToRGB(temperature), temperatureStrength);
